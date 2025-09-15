cf0b525d566a067c6415d8483af9a3b7d3393b8a
waseemashraf1 <waseemashraf@team.seerox.com>
2025-09-15 09:56:49 +0500

    add code pub work remove LINUX

--- scripts/packages.dart
+++ scripts/packages.dart
@@ -4,7 +4,7 @@ const packages = [
   'quill_native_bridge_platform_interface',
   'quill_native_bridge_android',
   'quill_native_bridge_ios',
-  'quill_native_bridge_linux',
+  // 'quill_native_bridge_linux',
   'quill_native_bridge_macos',
   'quill_native_bridge_web',
   'quill_native_bridge_windows',
